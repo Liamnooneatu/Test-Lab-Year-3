9902b4c612401fcec0e313f40d8a6f6984c56f59
G00420041@atu.ie <G00420041@atu.ie>
2024-11-05 10:04:38 +0000

    started testlab

---
+++ src/main/java/com/example/testlab/TestLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.testlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLabApplication.class, args);
+    }
+
+}
